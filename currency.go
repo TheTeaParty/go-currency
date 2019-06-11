fc48eb5dd18ed17ec678b585b792cb31def41b9f
Vlad <vlad@witchery.io>
2019-06-11 04:06:00 +0400

    tokenizer added

--- currency.go
+++ currency.go
@@ -6,7 +6,9 @@ type Currency struct {
 }
 
 var currencies = map[string]*Currency{
-	"AMD": {Code: "AMD", Fraction: 2},
-	"USD": {Code: "USD", Fraction: 2},
-	"RUB": {Code: "RUB", Fraction: 2},
+	"AMD":           {Code: "AMD", Fraction: 2},
+	"USD":           {Code: "USD", Fraction: 2},
+	"RUB":           {Code: "RUB", Fraction: 2},
+	"TOKENIZER-FUN": {Code: "TOKENIZER-FUN", Fraction: 2},
+	"TOKENIZER":     {Code: "TOKENIZER", Fraction: 2},
 }
